9e9a25252f33b1552cc46b97c88b9cd20e25dd8e
Tim Flood <tflood9@gmail.com>
2021-08-23 15:01:20 -0400

    diaplay letters

--- src/App.js
+++ src/App.js
@@ -3,15 +3,49 @@ import Header from "./components/Header";
 import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
+import React, { useState, useEffect } from "react";
+
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode > 65 && keyCode < 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            //showNotification
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            //showNotification
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
-        <WrongLetters />
-        <Word />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
